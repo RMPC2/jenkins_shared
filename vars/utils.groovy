25c137a2da2ab598dad578ee34f667b4227c4955
Thomas Campbell <thomas.campbell@ecs.co.uk>
2022-10-20 13:04:09 +0100

    updated library

---
+++ vars/utils.groovy
@@ -0,0 +1,10 @@
+
+def printFromFunction() {
+    println("I am printing from function")
+}
+
+def replaceString() {
+    def text = readFile file: "index.html"
+    text  = text.replaceAll("%BUILD_NUMBER%", "${BUILD_NUMBER}")
+    writeFile file: "index.html", text: text
+}
